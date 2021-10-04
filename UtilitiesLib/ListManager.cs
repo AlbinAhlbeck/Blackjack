173b46706f9f728341ee0a208e11f0e77af8fdc9
AlbinAhlbeck <albin.ahlbeck@gmail.com>
2021-10-04 16:44:20 +0200

    Initial commit

---
+++ UtilitiesLib/ListManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilitiesLib
+{
+    public abstract class ListManager<T> : IListManager<T>
+    {
+        public abstract int Count { get; }
+
+        public abstract bool Add(T aType);
+        public abstract bool ChangeAt(T aType, string anIndex);
+        public abstract bool CheckIndex(string index);
+        public abstract void DeleteAll();
+        public abstract bool DeleteAt(string anIndex);
+        public abstract T GetAt(string anIndex);
+        public abstract string[] ToStringArray();
+        public abstract List<string> ToStringList();
+    }
+}
---
+++ UtilitiesLib/Parser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace UtilitiesLib
+{
+    public static class Parser
+    {
+        public static int ParseString(string anString)
+        {
+            int number;
+            bool success = int.TryParse(anString, out number);
+            if (success)
+            {
+                return number;
+            }
+            else
+            {
+                throw new Exception("Could not parse");
+            }
+                          
+        }
+
+        public static int ParseString(string anString, int highLimit, int lowLimit)
+        {
+            int number;
+
+            bool success = int.TryParse(anString, out number);
+            if (success)
+            {
+                if (number < highLimit && number > lowLimit)
+                {
+                    return number;
+                }
+            }
+            
+            throw new Exception("Could not parse");
+
+        }
+
+        public static string ParseInt(int anInt)
+        {
+            return anInt.ToString();
+        }
+
+        public static string ParseInt(int anInt, int lowLimit, int highLimit)
+        {
+            if (anInt < highLimit && anInt > lowLimit)
+            {
+                return anInt.ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+    }
+}
