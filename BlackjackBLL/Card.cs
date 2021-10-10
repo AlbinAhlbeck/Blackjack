cf87887c0b04132973dc5cb8c978a41679604199
marcus97svensson <62559140+marcus97svensson@users.noreply.github.com>
2021-10-10 13:32:47 +0200

    Decktest

---
+++ BlackjackBLL/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Card
+{
+	private string face;
+	private Suite suite;
+
+	public Card(string cardFace, Suite cardSuite)
+	{
+		face = cardFace;
+		suite = cardSuite;
+	}
+
+	public override string toString()
+	{
+		return face + " of " + suite;
+	}
+}
