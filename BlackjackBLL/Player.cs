7211493ddade4dee346e0c4c3a64ab3ea8e01ec3
AlbinAhlbeck <albin.ahlbeck@gmail.com>
2021-10-11 13:22:36 +0200

    player

--- BlackjackBLL/Player.cs
+++ BlackjackBLL/Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlackjackBLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,66 @@ namespace Blackjack
 {
     class Player
     {
+        private Hand hand;
+        private bool isFinished;
+        private string name;
+        private string playerID;
+        private bool winner;
+
+        public Player(Hand hand, string name, string playerID)
+        {
+            this.name = name;
+            this.playerID = playerID;
+            this.hand = hand;
+        }
+
+        public Hand Hand
+        {
+            get
+            {
+                return hand;
+            }
+            set
+            {
+                hand = value;
+            }
+        }
+
+        public bool IsFinished
+        {
+            get
+            {
+                return isFinished;
+            }
+            set
+            {
+                isFinished = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string PlayerID
+        {
+            get
+            {
+                return playerID;
+            }
+            set
+            {
+                playerID = value;
+            }
+        }
+
     }
 }
